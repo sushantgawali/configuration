2edcdad18c7240e889db89526e13536c0a4d80ed
Filip Jukic <filip@jukic.me>
2014-11-28 20:07:54 +0100

    install runit nginx script

--- docker/edxapp/init.sh
+++ docker/edxapp/init.sh
@@ -5,5 +5,4 @@ cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./man
 . /edx/app/edxapp/edxapp_env
 cd /edx/app/edxapp/edx-platform && paver update_assets cms --settings=docker_multi
 cd /edx/app/edxapp/edx-platform && paver update_assets lms --settings=docker_multi
-/usr/sbin/nginx &
 /sbin/my_init
