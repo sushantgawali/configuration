3030403d91cc6fb5e6d414302234fde3ac815586
Filip Jukic <filip@jukic.me>
2014-11-28 17:29:39 +0100

    update edxapp init script

--- docker/edxapp/init.sh
+++ docker/edxapp/init.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
-cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./manage.py lms syncdb --migrate --noinput --settings aws
-cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./manage.py cms syncdb --migrate --noinput --settings aws
-source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform && paver update_assets cms --settings=aws
-source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform && paver update_assets lms --settings=aws
+cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./manage.py lms syncdb --migrate --noinput --settings docker_multi
+cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./manage.py cms syncdb --migrate --noinput --settings docker_multi
+source /edx/app/edxapp/edxapp_env
+cd /edx/app/edxapp/edx-platform && paver update_assets cms --settings=docker_multi
+cd /edx/app/edxapp/edx-platform && paver update_assets lms --settings=docker_multi
 /usr/sbin/nginx &
 /sbin/my_init
