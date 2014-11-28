8efd21b390086f56c18fb014ab647fa3c963ef2b
Filip Jukic <filip@jukic.me>
2014-11-28 19:59:48 +0100

    correctly source the environment

--- docker/edxapp/init.sh
+++ docker/edxapp/init.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./manage.py lms syncdb --migrate --noinput --settings docker_multi
 cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./manage.py cms syncdb --migrate --noinput --settings docker_multi
-source /edx/app/edxapp/edxapp_env
+. /edx/app/edxapp/edxapp_env
 cd /edx/app/edxapp/edx-platform && paver update_assets cms --settings=docker_multi
 cd /edx/app/edxapp/edx-platform && paver update_assets lms --settings=docker_multi
 /usr/sbin/nginx &
