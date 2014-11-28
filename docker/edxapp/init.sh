803347f445a198443e072c5c3b976908ff9aa0ac
Filip Jukic <filip@jukic.me>
2014-11-28 20:15:12 +0100

    pass env variables with sudo

--- docker/edxapp/init.sh
+++ docker/edxapp/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./manage.py lms syncdb --migrate --noinput --settings docker_multi
-cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./manage.py cms syncdb --migrate --noinput --settings docker_multi
+cd /edx/app/edxapp/edx-platform && sudo -u www-data -E /edx/bin/python.edxapp ./manage.py lms syncdb --migrate --noinput --settings docker_multi
+cd /edx/app/edxapp/edx-platform && sudo -u www-data -E /edx/bin/python.edxapp ./manage.py cms syncdb --migrate --noinput --settings docker_multi
 . /edx/app/edxapp/edxapp_env
 cd /edx/app/edxapp/edx-platform && paver update_assets cms --settings=docker_multi
 cd /edx/app/edxapp/edx-platform && paver update_assets lms --settings=docker_multi
