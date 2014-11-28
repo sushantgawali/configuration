80b372b3759b7a79c8c4c5599fd533d2930c647d
Filip Jukic <filip@jukic.me>
2014-11-28 16:23:55 +0100

    noinput migrations/syncdb

--- docker/edxapp/init.sh
+++ docker/edxapp/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./manage.py lms syncdb --migrate --settings aws
-cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./manage.py cms syncdb --migrate --settings aws
+cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./manage.py lms syncdb --migrate --noinput --settings aws
+cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./manage.py cms syncdb --migrate --noinput --settings aws
 source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform && paver update_assets cms --settings=aws
 source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform && paver update_assets lms --settings=aws
 /usr/sbin/nginx &
