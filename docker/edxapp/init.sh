2dedcb65619cfa8407bb98923923ec2986839348
Morgan Robertson <robertson.md7@gmail.com>
2015-10-16 10:08:48 -0400

    Don't run paver on edxapp container startup.

--- docker/edxapp/init.sh
+++ docker/edxapp/init.sh
@@ -2,7 +2,4 @@
 set -e
 cd /edx/app/edxapp/edx-platform && sudo -u www-data -E /edx/bin/python.edxapp ./manage.py lms syncdb --migrate --noinput --settings docker_multi
 cd /edx/app/edxapp/edx-platform && sudo -u www-data -E /edx/bin/python.edxapp ./manage.py cms syncdb --migrate --noinput --settings docker_multi
-. /edx/app/edxapp/edxapp_env
-cd /edx/app/edxapp/edx-platform && paver update_assets cms --settings=docker_multi
-cd /edx/app/edxapp/edx-platform && paver update_assets lms --settings=docker_multi
 /sbin/my_init
