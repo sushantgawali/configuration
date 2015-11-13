1358d1352a4a4d4be8f939c5cdc0ceeba2185dd7
Morgan Robertson <robertson.md7@gmail.com>
2015-11-13 13:42:28 -0500

    Remove init.sh (replaced by configure script).

--- docker/edxapp/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-cd /edx/app/edxapp/edx-platform && sudo -u www-data -E /edx/bin/python.edxapp ./manage.py lms syncdb --migrate --noinput --settings docker_multi
-cd /edx/app/edxapp/edx-platform && sudo -u www-data -E /edx/bin/python.edxapp ./manage.py cms syncdb --migrate --noinput --settings docker_multi
-/sbin/my_init
